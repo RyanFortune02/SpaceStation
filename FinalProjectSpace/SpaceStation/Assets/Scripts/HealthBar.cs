33069b4d13847244d4fd5599830a8271431c5833
Ryan <r.fortune25@ncf.edu>
2024-05-04 02:44:21 -0400

    Finished Cryo Setup
    - start hallway with floating objects

---
+++ FinalProjectSpace/SpaceStation/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=6U_OZkFtyxY&t=123s   Here is video for health bar to use
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
