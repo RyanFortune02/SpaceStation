dd79891bc7c124d6305917678b9cef12b403b898
Ryan <r.fortune25@ncf.edu>
2024-05-04 23:03:02 -0400

    Created SpaceHallway Scene
    - Scene will be used for finding food for health and escaping randomly floating objects
    - Issue with windows and corners of walls of falling out of map on CryoScene and SpaceHallway
         - Add colliders around all sides of vessel

--- FinalProjectSpace/SpaceStation/Assets/Scripts/TextManager.cs
+++ FinalProjectSpace/SpaceStation/Assets/Scripts/TextManager.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;  // Include the SceneManager namespace
 
 public class TextManager : MonoBehaviour
 {
     public Text MessageText;
-
 
     public void SetMessageText(string message)
     {
@@ -15,11 +15,31 @@ public class TextManager : MonoBehaviour
 
     void Start()
     {
-        SetMessageText("Welcome! You have just woken from Cyrosleep! Look to your left for instructions."); // Welcome message
+        //Used for getting current active scene to display a start up message in the UI
+        //https://gamedev.stackexchange.com/questions/153707/how-to-get-current-scene-name 
+        //Unity Docs for future reference: https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.GetActiveScene.html
+        var currentScene = SceneManager.GetActiveScene();
+        var currentSceneName = currentScene.name;
+
+        if (currentSceneName == "Cryoscene")
+        {
+            SetMessageText("Welcome! You have just woken from Cryosleep! Look to your left for instructions.");
+        }
+        else if (currentSceneName == "SpaceHallway")
+        {
+            SetMessageText("Escape randomly floating objects\r\nFind food resources for health \r\n");
+        }
+        else  //
+        {
+            SetMessageText("Welcome to the end scene");
+        }
     }
 
     void Update()
     {
-        
+
     }
 }
+
+
+
